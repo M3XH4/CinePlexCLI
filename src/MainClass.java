fb0d13573334987553889b65bc0c8e1ceed06c3f
M3XH4 <mechasean23123@gmail.com>
2024-06-03 01:27:38 +0800

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,9 @@
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println(ColorManager.TEXT_BLACK + ColorManager.BACKGROUND_RED + "=====================================================================" + ColorManager.RESET);
+        System.out.println(Titles.title1);
+        System.out.println(ColorManager.TEXT_BLACK + ColorManager.BACKGROUND_RED + "=====================================================================" + ColorManager.RESET);
+        System.out.println();
+
+    }
+}
---
+++ src/Titles.java
@@ -0,0 +1,9 @@
+public class Titles {
+    static String title1 =
+            ColorManager.BACKGROUND_RED + "\t ██████╗██╗███╗   ██╗███████╗██████╗ ██╗     ███████╗██╗  ██╗    " + ColorManager.RESET + "\n" +
+            ColorManager.BACKGROUND_RED + "\t██╔════╝██║████╗  ██║██╔════╝██╔══██╗██║     ██╔════╝╚██╗██╔╝    " + ColorManager.RESET + "\n" +
+            ColorManager.BACKGROUND_RED + "\t██║     ██║██╔██╗ ██║█████╗  ██████╔╝██║     █████╗   ╚███╔╝     " + ColorManager.RESET + "\n" +
+            ColorManager.BACKGROUND_RED + "\t██║     ██║██║╚██╗██║██╔══╝  ██╔═══╝ ██║     ██╔══╝   ██╔██╗     " + ColorManager.RESET + "\n" +
+            ColorManager.BACKGROUND_RED + "\t╚██████╗██║██║ ╚████║███████╗██║     ███████╗███████╗██╔╝ ██╗    " + ColorManager.RESET + "\n" +
+            ColorManager.BACKGROUND_RED + "\t ╚═════╝╚═╝╚═╝  ╚═══╝╚══════╝╚═╝     ╚══════╝╚══════╝╚═╝  ╚═╝    " + ColorManager.RESET;
+}
